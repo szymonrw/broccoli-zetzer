d8e9e2effa32c2758b3553576c06d8b4ccf97de9
Szymon Witamborski <szywon@szywon.pl>
2014-04-18 15:45:09 +0200

    Test with broccoli 0.9.

--- Brocfile.js
+++ Brocfile.js
@@ -2,16 +2,14 @@
 
 var stencil = require("./index");
 
-module.exports = function (broccoli) {
-  var pages = "node_modules/grunt-stencil/spec/fixtures";
-  var partials = "node_modules/grunt-stencil/spec/includes";
-  var templates = "node_modules/grunt-stencil/spec/templates";
+var pages = "node_modules/grunt-stencil/spec/fixtures";
+var partials = "node_modules/grunt-stencil/spec/includes";
+var templates = "node_modules/grunt-stencil/spec/templates";
 
-  var site = stencil({
-    pages: pages,
-    partials: partials,
-    templates: templates
-  });
+var site = stencil({
+  pages: pages,
+  partials: partials,
+  templates: templates
+});
 
-  return site;
-}
+module.exports = site;
