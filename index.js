3003942a030170732f11b3edf200197ee00bddfe
Szymon Witamborski <szywon@szywon.pl>
2014-06-01 23:16:20 +0100

    Use broccoli-dep-filter.

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ var quick_temp = require("quick-temp");
 var map_series = require("promise-map-series");
 var walk_sync = require("walk-sync");
 
+var filter = require("broccoli-dep-filter");
+
 var parse_setup = require("grunt-stencil/lib/parse");
 var compilers_setup = require("grunt-stencil/lib/compilers");
 var process_file_setup = require("grunt-stencil/lib/process_file");
@@ -23,15 +25,12 @@ var META_DATA_SEPARATOR = /\r?\n\r?\n/;
 module.exports = stencil;
 
 function stencil (trees, options) {
-  var pages_root = trees.pages;
-  var partials_root = trees.partials;
-  var templates_root = trees.templates;
+  var pages_tree = trees.pages;
+  var partials_tree = trees.partials;
+  var templates_tree = trees.templates;
 
   options = options     || {};
   var env = options.env || {};
-
-  var tmp = {};
-  quick_temp.makeOrReuse(tmp, "path");
 
   var parse = parse_setup(META_DATA_SEPARATOR);
 
@@ -40,51 +39,49 @@ function stencil (trees, options) {
     compilers:    [dot_compiler, markdown_compiler]
   });
 
-  return {
-    read:    read,
-    cleanup: cleanup
-  };
+  return filter({
+    trees: {
+      pages: pages_tree,
+      partials: partials_tree,
+      templates: templates_tree
+    },
+    iterated: ["pages"],
+    init: init,
+    extensions: ["html", "md"],
+    target: "html",
+    read: false
+  });
 
-  function read (read_tree) {
-    return map_series([pages_root, partials_root, templates_root], read_tree).then(run);
+  function init (roots) {
+    var partials  = { root: roots.partials, paths: walk_sync(roots.partials) };
+    var templates = { root: roots.templates, paths: walk_sync(roots.templates) };
 
-    function run (paths) {
-      var pages     = { root: paths[0], paths: walk_sync(paths[0]) };
-      var partials  = { root: paths[1], paths: walk_sync(paths[1]) };
-      var templates = { root: paths[2], paths: walk_sync(paths[2]) };
-
-      var process_file = process_file_setup({
-        compile:            compile,
-        read_header:        _.compose(parse.header, read_file),
-        find_closest_match: find_closest_match,
-        options: {
-          env:       env,
-          pages:     pages,
-          partials:  partials,
-          templates: templates
-        }
-      });
-
-      pages.paths.filter(function (path) {
-        return /\.(html|md)$/.test(path);
-      }).forEach(function (path) {
-        var result = process_file(path).toString();
-        path = path.replace(/(\.[^.]+)+$/, ".html");
-        save_file(join_paths(tmp.path, path), result);
-      });
-
-      return tmp.path;
-
-      function find_closest_match (tree, name) {
-        // Workaround because stencil passes "." for pages here...
-        tree = tree === "." ? pages : tree;
-
-        var path = tree.paths.filter(function (path) {
-          return path.indexOf(name) === 0;
-        })[0];
-
-        return join_paths(tree.root, path);
+    var process_file = process_file_setup({
+      compile:            compile,
+      read_header:        _.compose(parse.header, read_file),
+      find_closest_match: find_closest_match,
+      options: {
+        env:       env,
+        partials:  partials,
+        templates: templates
       }
+    });
+
+    return process;
+
+    function process (path) {
+      return process_file(path).toString();
+    }
+
+    function find_closest_match (tree, name) {
+      // Stencil for pages passes "." which should be changed
+      if (tree === ".") return name;
+
+      var path = tree.paths.filter(function (path) {
+        return path.indexOf(name) === 0;
+      })[0];
+
+      return join_paths(tree.root, path);
     }
   }
 
@@ -95,9 +92,4 @@ function stencil (trees, options) {
 
 function read_file (path) {
   return fs.readFileSync(path, "utf8");
-}
-
-function save_file (path, contents) {
-  mkdirp.sync(dirname(path));
-  return fs.writeFileSync(path, contents, "utf8");
 }
